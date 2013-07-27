c167ebe57d7f7ee1ddc68bfe1656dba1f6bf9e1f
Michael Sheets <git@whitefalls.org>
2013-07-27 03:11:52 -0500

    Use version 1.8 of ruby for bundle items
    10.7 and 10.8 only include ruby 1.8, so all bundle items have been written to work with that. Optionally supporting ruby 1.9 and 2.0 is problematic as these versions are not fully backwards compatible.
    
    #ignore

--- Support/browser.rb
+++ Support/browser.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby
 PROJECT_ROOT = ARGV[0] # Where to browse for images
 RESOURCES_DIR = "#{ENV['TM_BUNDLE_SUPPORT']}/Resources" # Where to find ImageBrowser's images
 require "#{ENV['TM_BUNDLE_SUPPORT']}/image_size.rb"
